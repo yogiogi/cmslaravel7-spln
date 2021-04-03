3a67053fbac83d3899cad84ab9aeb37ceb272c83
yogiogi <masogiogi@gmail.com>
2021-04-04 00:14:43 +0800

    push changing 3april-malam

--- resources/views/emailku.blade.php
+++ resources/views/emailku.blade.php
@@ -1,2 +1,13 @@
-<h1>Hi, Yogi</h1>
-l<p>Sending Mail from Laravel.</p>
+<h1>Halo, Pelanggan S-PLN</h1>
+<br>
+<p>Berikut kami kirimkan bukti registrasi layanan PT S-PLN yang bisa diunduh pada attachment email dibawah.</p>
+<p>Selanjutnya Bapak/Ibu/Sdr dipersilahkan untuk melakukan pembayaran selambat-lambatnya 14 hari sejak diterimanya email konfirmasi ini</p>
+<br>
+<p>Terimakasih telah mempercayakan kami, </p>
+<br>
+<br>
+<p>Hormat Kami,</p>
+<br>
+<p>PR S-PLN</p>
+
+
